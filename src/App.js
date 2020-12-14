1585c3653f6b3ed9a902a903768b260debe16717
Anikets08 <31aniket.singh@gmail.com>
2020-12-14 12:35:41 +0530

    styledComponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import React from 'react';
+import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
+    <GlobalStyle/>
       <AboutUs/>
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import home1 from '../images/home1.png';
+import styled from 'styled-components';
+
 function AboutSection() {
     return (
-        <div>
-            <div className="discription">
+        <About>
+            <Discription>
                 <div className="title">
                     <div className="hide">
                         <h2>We work to make</h2>
@@ -18,12 +20,34 @@ function AboutSection() {
                 <p>Contact us for any photography or videography ideas that you have . We have
                 proffesional with amazing skills</p>
                 <button>Contact us</button> 
-            </div>  
-            <div className="image">
+            </Discription>  
+            <Img>
                 <img src={home1} alt="cameraGuy"/>
-            </div>  
-        </div>
-    )
+            </Img>  
+        </About>
+    );
+};
+
+//styled components
+const About = styled.div`
+min-height: 90vh;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 5rem 10rem;
+color: white;
+`
+const Discription = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2{
+    font-weight: lighter;
 }
+`
+
+const Img = styled.div`
+flex: 1;
+`
+
 
 export default AboutSection
